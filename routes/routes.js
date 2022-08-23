5aed876a273818f8d3bc84788a7705682f9c8842
Nerms <Nerms@Nermins-MacBook-Pro.local>
2022-08-23 00:24:35 -0500

    heroku commit

---
+++ routes/routes.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+const path = require('path');
+
+module.exports = app =>{
+    //setup notes variable
+    fs.readFile("db/db.json",)
+}
--- server.js
+++ server.js
@@ -1,20 +1,70 @@
 const express = require('express');
 const path = require('path');
 const fs = require("fs")
-// const util = require('util')
+const util = require('util')
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
+
 // Middleware for parsing JSON and urlencoded form data
+//data parsing
+app.use(express.static('./develop/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
 
-// GET Route for homepage
+//api route get req
+app.get("/api/notes", (req,res) => {
+  readFileAsync("./db/db.json", "utf-8").then(function(data) {
+    notes =[].concat(JSON.parse(data))
+    res.json(notes);
+  })
+});
+
+app.post("/api/notes", (req,res)=>{
+  const note = req.body
+  readFileAsync("./develop/db/db.json", "utf-8").then(function(data){
+    notes =[].concat(JSON.parse(data));
+    note.id =note.length + 1
+    notes.push(note);
+    return notes
+  }).then(function(notes) {
+    writeFileAsync('./develop/db/db.json'.JSON.stringify(notes))
+    res.json(note);
+  })
+});
+
+app.delete('/api/notes/:id', (req,res)=>{
+  const idToDelete = parseInt(req.params.id);
+  readFileAsync('./develop.db/db.json', 'utf-8').then(function(data){
+    const notes =[].concat(JSON.parse(data));
+    const newNotes = []
+    for(let i=0;i<notes.length;i++){
+      if(idToDelete !== notes[i].id){
+        newNotes.push(notes[i])
+      }
+    }
+    return newNotes
+  }).then(function(notes) {
+    writeFileAsync('./develop/db/db.json', json.stringify(notes))
+    res.send('saved!')
+  })
+})
+
+// GET Route for html
 app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, './develop/public/index.html'))
+  res.sendFile(path.join(__dirname, '/develop/public/index.html'))
 );
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/develop/public/index.html'))
+);
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/develop/public/notes.html'))
+);
+
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
